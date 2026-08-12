b8b2fbfb97d53243e1717fbedf0250e17557166b
yasshuk <yasminkhan28102006@gmail.com>
2026-08-12 10:13:27 +0530

    Updated registration validation

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -10,53 +9,55 @@ import { RouterLink } from '@angular/router';
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register {
 
+export class Register {
   fullName = '';
   username = '';
   contactNumber = '';
   password = '';
   confirmPassword = '';
   selectedRole = 'staff';
-
   showPassword = false;
   showConfirmPassword = false;
 
+  submitted = false;
+  
   register() {
+  this.submitted = true;
 
-    if (
-      !this.fullName ||
-      !this.username ||
-      !this.contactNumber ||
-      !this.password ||
-      !this.confirmPassword
-    ) {
-      alert('Please fill in all fields.');
-      return;
-    }
-
-    if (this.contactNumber.length !== 10) {
-      alert('Please enter a valid 10-digit contact number.');
-      return;
-    }
-
-    if (this.password.length < 6) {
-      alert('Password must contain at least 6 characters.');
-      return;
-    }
-
-    if (this.password !== this.confirmPassword) {
-      alert('Passwords do not match.');
-      return;
-    }
-
-    console.log({
-      fullName: this.fullName,
-      username: this.username,
-      contactNumber: this.contactNumber,
-      role: this.selectedRole
-    });
-
-    alert('Form validated successfully. Backend connection will be added next.');
+  // Required field validation
+  if (
+    !this.fullName ||
+    !this.username ||
+    !this.contactNumber ||
+    !this.password ||
+    !this.confirmPassword
+  ) {
+    return;
   }
+
+  // Contact validation
+  if (this.contactNumber.length !== 10) {
+    return;
+  }
+
+  // Password validation
+  if (this.password.length < 6) {
+    return;
+  }
+
+  // Confirm password validation
+  if (this.password !== this.confirmPassword) {
+    return;
+  }
+
+  console.log({
+    fullName: this.fullName,
+    username: this.username,
+    contactNumber: this.contactNumber,
+    role: this.selectedRole
+  });
+
+  alert('Account Created successfully.');
+}
 }
