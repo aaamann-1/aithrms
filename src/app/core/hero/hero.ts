a01282f859c522fad1ab7b05cc440181293bb0b6
sayeedmomin09-cyber <sayeedmomin09@gmail.com>
2026-08-13 20:16:49 +0530

    updated dashboard frontend

--- src/app/core/hero/hero.ts
+++ src/app/core/hero/hero.ts
@@ -1,17 +1,69 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './hero.html',
   styleUrl: './hero.css'
 })
 export class Hero {
+
+  showDemoModal = false;
+
+  demoSubmitted = false;
+
+  demoName = '';
+  demoEmail = '';
+  demoMobile = '';
+  demoEmployeeSize = '';
+
+  openDemoModal(): void {
+    this.showDemoModal = true;
+  }
+
+  closeDemoModal(): void {
+    this.showDemoModal = false;
+  }
+
+  submitDemo(): void {
+
+    if (
+      !this.demoName ||
+      !this.demoEmail ||
+      !this.demoMobile ||
+      !this.demoEmployeeSize
+    ) {
+      alert('Please fill all the details.');
+      return;
+    }
+
+    this.demoSubmitted = true;
+
+    console.log('Demo Request:', {
+      name: this.demoName,
+      email: this.demoEmail,
+      mobile: this.demoMobile,
+      employeeSize: this.demoEmployeeSize
+    });
+
+    // Close popup if it is open
+    this.showDemoModal = false;
+
+    alert('Demo request submitted successfully!');
+
+    // Clear form
+    this.demoName = '';
+    this.demoEmail = '';
+    this.demoMobile = '';
+    this.demoEmployeeSize = '';
+  }
 
 }
