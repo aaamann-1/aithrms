3003d0905850564863d2ec888ebceb16e08c9490
aaamann-1 <combatant004@gmail.com>
2026-08-26 15:45:08 +0530

    sidebar admin and app routes ts

--- src/app/admin/sidebar/sidebar.ts
+++ src/app/admin/sidebar/sidebar.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   imports: [],
+<<<<<<< Updated upstream:src/app/admin/sidebar/sidebar.ts
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
+=======
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
+>>>>>>> Stashed changes:src/app/admin/sidebar/sidebar.component.ts
 })
 export class Sidebar {}
